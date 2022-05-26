c5ddadaa0db3461e3d359748f9e32a0579650e64
Ayagoumi <agoumi94@gmail.com>
2022-05-26 01:37:53 +0100

    Add: useOnScreen hook

---
+++ Hooks/useOnScreen/useOnScreen.js
@@ -0,0 +1,26 @@
+/*
+  useOnScreen hook - is used to detect when an element is in the viewport
+  Example:
+    const visible = useOnScreen(headerTwoRef, "-100px")
+*/
+
+import { useEffect, useState } from "react";
+
+export default function useOnScreen(ref, rootMargin = "0px") {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (ref.current == null) return;
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsVisible(entry.isIntersecting),
+      { rootMargin }
+    );
+    observer.observe(ref.current);
+    return () => {
+      if (ref.current == null) return;
+      observer.unobserve(ref.current);
+    };
+  }, [ref.current, rootMargin]);
+
+  return isVisible;
+}
