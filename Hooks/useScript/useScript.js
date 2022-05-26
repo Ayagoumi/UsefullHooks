54734f1b4ad74df01b9ab358fbc2b25b03d2246d
Ayagoumi <agoumi94@gmail.com>
2022-05-26 01:32:20 +0100

    Add: useScript hook

---
+++ Hooks/useScript/useScript.js
@@ -0,0 +1,57 @@
+/*
+  The useScript() hook allows appending script tags to your document 
+  from inside React components. 
+  The state variable returns a status that can have one of 
+  the following values: idle, loading, ready, error.
+  Example:
+    const status = useScript("https://example.com/script.js");
+*/
+
+import { useEffect, useState } from "react";
+
+export function useScript(src) {
+  const [status, setStatus] = useState(src ? "loading" : "idle");
+
+  useEffect(() => {
+    if (!src) {
+      setStatus("idle");
+      return;
+    }
+
+    let script = document.querySelector(`script[src="${src}"]`);
+
+    if (!script) {
+      script = document.createElement("script");
+      script.src = src;
+      script.async = true;
+      script.setAttribute("data-status", "loading");
+      document.body.appendChild(script);
+      const setAttributeFromEvent = (event) => {
+        script?.setAttribute(
+          "data-status",
+          event.type === "load" ? "ready" : "error"
+        );
+      };
+
+      script.addEventListener("load", setAttributeFromEvent);
+      script.addEventListener("error", setAttributeFromEvent);
+    } else {
+      setStatus(script.getAttribute("data-status"));
+    }
+
+    const setStateFromEvent = (event) =>
+      setStatus(event.type === "load" ? "ready" : "error");
+
+    script.addEventListener("load", setStateFromEvent);
+    script.addEventListener("error", setStateFromEvent);
+
+    return () => {
+      if (script) {
+        script.removeEventListener("load", setStateFromEvent);
+        script.removeEventListener("error", setStateFromEvent);
+      }
+    };
+  }, [src]);
+
+  return status;
+}
