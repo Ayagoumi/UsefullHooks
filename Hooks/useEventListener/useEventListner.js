7aa93958381082961e71149ebcd8feeda9121ce3
Ayagoumi <agoumi94@gmail.com>
2022-05-26 01:40:19 +0100

    Add: useEventListener hook

---
+++ Hooks/useEventListener/useEventListner.js
@@ -0,0 +1,30 @@
+/*
+  useEventListener hook - is used to listen to events
+  Example:
+    const [key, setKey] = useState("")
+    useEventListener("keydown", e => {
+      setKey(e.key)
+    })
+*/
+
+import { useEffect, useRef } from "react";
+
+export default function useEventListener(
+  eventType,
+  callback,
+  element = window
+) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (element == null) return;
+    const handler = (e) => callbackRef.current(e);
+    element.addEventListener(eventType, handler);
+
+    return () => element.removeEventListener(eventType, handler);
+  }, [eventType, element]);
+}
