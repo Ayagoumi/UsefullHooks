c3aae20a66268967ff13672b805d2d0e1d7b2346
Ayagoumi <agoumi94@gmail.com>
2022-05-26 01:41:42 +0100

    Add: useCookie hook

---
+++ Hooks/useCookie/useCookie.js
@@ -0,0 +1,32 @@
+/*
+  useCookie stores a cookie in the browser.
+  Example:
+    const [value, update, remove] = useCookie("name", "John")
+*/
+
+import { useState, useCallback } from "react";
+import Cookies from "js-cookie";
+
+export default function useCookie(name, defaultValue) {
+  const [value, setValue] = useState(() => {
+    const cookie = Cookies.get(name);
+    if (cookie) return cookie;
+    Cookies.set(name, defaultValue);
+    return defaultValue;
+  });
+
+  const updateCookie = useCallback(
+    (newValue, options) => {
+      Cookies.set(name, newValue, options);
+      setValue(newValue);
+    },
+    [name]
+  );
+
+  const deleteCookie = useCallback(() => {
+    Cookies.remove(name);
+    setValue(null);
+  }, [name]);
+
+  return [value, updateCookie, deleteCookie];
+}
